fb72333958655704cad91b255fb907589ad31c9f
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-06-11 19:35:22 -0300

    fix folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return ()
-  
-     
+  return (
+    <h1>SportCenter</h1>
+  )
 }
+
 export default App
