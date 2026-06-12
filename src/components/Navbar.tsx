796d80063bef1df5cc0b8a2362c348916a7a45f2
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-06-11 21:13:28 -0300

    feat: navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,26 @@
-function Navbar(){
-    return <nav>Navbar</nav>
+import "./Navbar.css";
+import perfilIcono from "../assets/profile-svgrepo-com.svg";
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="/">
+          <h2>SportCenter</h2>
+        </a>
+      </div>
+      <div className="navbar-links">
+        <a href="">Inicio</a>
+        <a href="">Servicios</a>
+        <a href="">Mis turnos</a>
+      </div>
+      <div className="navbar-auth">
+        <a href="">
+          <img src={perfilIcono} alt="Icono usuario" width={24} height={24} />
+        </a>
+        <button className="nav-button">Reservar</button>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -2,14 +2,14 @@
 
 function Hero() {
     return (
-        <>
+        <section>
             <h1>Sport Center</h1>
             <p>Centro deportivo con canchas y profesionales </p>
             <button>Reservar Turno</button>
 
 
 
-        </>
+        </section>
     )
 }
 
