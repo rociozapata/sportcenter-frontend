72a1b0a0343d42395ad9f3c57ef3b18c86dba326
rociozapata <rocio.zapata003@gmail.com>
2026-06-15 19:43:45 -0300

    fix:services.css and services.jsx

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,50 +1,101 @@
-
+import "./Services.css"
 
 const service = [
     {
         key: "tennis",
         title: "Clases de Tenis",
         description: "Mejorá tu técnica y desarrollá tu juego en un ambiente amigable y profesional. Clases grupales e individuales.",
-        img: "src/assets/service-tenis.jpg"
+        size: "wide"
+    },
+
+    {
+        key: "funcional",
+        title: "Entrenamiento Funcional",
+        description: "El entrenamiento que se adapta a tu vida. Ideales para potenciar tu rendimiento deportivo o mejorar tu salud.",
+        size: "narrow"
+    },
+
+    {
+        key: "crossfit",
+        title: "Crossfit",
+        description: "Superá tus propias marcas en cada entrenamiento. Preparate para descubrir de lo que sos capaz.",
+        size: "narrow"
     },
 
     {
         key: "padel",
         title: "Clases de Pádel",
         description: "Ya seas principiante o un jugador experimentado, nuestras clases te llevarán al siguiente nivel. Clases individuales y grupales",
-        img: "src/assets/service-padel.jpg"
-    },
+        size: "wide"
+    }
+
+];
+
+const staff = [
     {
-        key: "funcional",
-        title: "Entrenamiento Funcional",
-        description: "Acá va el chamuyo copado sobre funcional cuando lo busques.",
-        img: "src/assets/service-funcional.jpg"
+        key: "",
+        name: "",
+        description: "",
+        size: ""
     },
 
     {
-        key: "crossfit",
-        title: "Crossfit",
-        description: "Acá va el chamuyo potente sobre Crossfit.",
-        img: "src/assets/service-crossfit.jpg"
+        key: "",
+        name: "",
+        description: "",
+        size: ""
+    },
+
+    {
+        key: "",
+        name: "",
+        description: "",
+        size: ""
+    },
+
+    {
+        key: "",
+        name: "",
+        description: "",
+        size: ""
     }
+
 ];
 
 function Services() {
     return (
-        <section className="service-container">
-            <div>
-            {service.map((s) => (
-                <article key={s.key} className="card-item">
-                    <img src={s.img} alt={s.title} />
+        <section className="services">
 
-                    <div className="card-content">
-                        <h3>{s.title}</h3>
-                        <p>{s.description}</p>
-                        <button className="btn btn-accent">Ver horarios</button>
-                    </div>
-                </article>
-            
-            ))}
+            <h2 className="services-header">Nuestras Clases</h2>
+
+            <div className="services-grid">
+                {service.map((s) => (
+                    <article
+                        key={s.key}
+                        className={`services-card services-card--${s.size} services-card--${s.key}`}>
+
+                        <div className="services-card-content">
+                            <h3 className="services-card-title">{s.title}</h3>
+                            <p className="services-card-description">{s.description}</p>
+                            <button className="btn btn-accent">Ver horarios</button>
+                        </div>
+                    </article>
+
+                ))}
+            </div>
+
+            <h2 className="services-header">Nuestro Staff</h2>
+
+            <div className="services-grid">
+                {staff.map((p) => (
+                    <article key={p.key} className={`services-card services-card--${p.size} services-card--{p.key}`}>
+                        <div className="services-card-content">
+                            <h3 className="services-card-title">{p.name}</h3>
+                            <p className="services-card-description">{p.description}</p>
+                        </div>
+                    </article>
+                ))}
+
             </div>
         </section>
 
